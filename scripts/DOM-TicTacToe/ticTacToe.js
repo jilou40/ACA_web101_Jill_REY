673e38528bf81ff182a86f6b96a9c349c3d64f4d
jilou40 <rey.jill@ymail.com>
2019-03-04 18:28:04 -0600

    tic

--- scripts/DOM-TicTacToe/ticTacToe.js
+++ scripts/DOM-TicTacToe/ticTacToe.js
@@ -1,3 +1,20 @@
-function java (){
-    alert("Page is loaded");
+var gameMarker = "X";
+
+function placeMark(id){
+    var cell = document.getElementById(id);
+        cell.innerHTML = gameMarker;
+
+    }
+
+
+
+function changeMarkerToX(){
+    gameMarker = "X";
+    console.log(gameMarker);
 }
+
+function changeMarkerToO() {
+    gameMarker = "O";
+    console.log(gameMarker);
+}
+
